e55a4542741f46e6f6750bf078e1da4892aa8116
Tony <twseco@hotmail.com>
2015-07-30 17:35:06 -0400

    Visual Changes etc

--- ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
+++ ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -1,12 +1,15 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.parse.FindCallback;
 import com.parse.ParseAnalytics;
@@ -65,58 +68,32 @@ public class ParseStarterProjectActivity extends AppCompatActivity {
 
 	}
 
-
-	private void loadFromParse(){
-//		ParseQuery<Component> componentQuery = Component.getQuery();
-//		ParseQuery<Incident> incidentQuery = Incident.getQuery();
-//
-//		componentQuery.findInBackground(new FindCallback<Component>() {
-//			@Override
-//			public void done(List<Component> componentList, ParseException e) {
-//				if (e ==null){
-//					ParseObject.pinAllInBackground((List<Component>) componentList,
-//							new SaveCallback() {
-//								@Override
-//								public void done(ParseException e) {
-//									if(e == null){
-//										if(!isFinishing()){
-//
-//										}else{
-//											Log.i("Component","Error:(");
-//										}
-//									}
-//								}
-//							});
-//				}else{
-//					Log.i("ComponentMore","Error;-;");
-//				}
-//			}
-//		});
-//
-//
-//		incidentQuery.findInBackground(new FindCallback<Incident>() {
-//			@Override
-//			public void done(List<Incident> incidentList, ParseException e) {
-//				if (e ==null){
-//					ParseObject.pinAllInBackground((List<Incident>) incidentList,
-//							new SaveCallback() {
-//								@Override
-//								public void done(ParseException e) {
-//									if(e == null){
-//										if(!isFinishing()){
-//
-//										}else{
-//											Log.i("Incident","Error:(");
-//										}
-//									}
-//								}
-//							});
-//				}else{
-//					Log.i("IncidentMore","Error;-;");
-//				}
-//			}
-//		});
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		if (id==R.id.info){
+			startActivity(new Intent(this, ComponentActivity.class ));
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
 
 	@Override
 	public void onResume(){
